5911916f5845887073bb82088f1a2e2a0d0cc555
hasanYigit <yigith@posta.mu.edu.tr>
2026-03-27 19:31:14 +0300

    MVC Mimarisi: Ekipmanlar için Model, Controller ve View katmanları oluşturuldu. Kiralık ekipmanlar mock data ve Bootstrap Grid sistemi ile listelendi. UI/UX Düzeltmesi: Odalar ve Ekipmanlar sayfalarındaki beyaz arka planla çakışan başlık metinlerinin renkleri düzeltilerek okunaklı hale getirildi.

---
+++ Controllers/EkipmanlarController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RezerveMuzikWeb.Models;
+using System.Collections.Generic;
+
+namespace RezerveMuzikWeb.Controllers
+{
+    public class EkipmanlarController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Veritabanı bağlanana kadar kullanacağımız sahte ekipman listesi
+            var sahteEkipmanlar = new List<Ekipman>
+            {
+                new Ekipman { Id = 1, EkipmanAdi = "Fender Stratocaster Elektro Gitar", SaatlikKiraUcreti = 100 },
+                new Ekipman { Id = 2, EkipmanAdi = "Marshall 100W Amfi", SaatlikKiraUcreti = 80 },
+                new Ekipman { Id = 3, EkipmanAdi = "Shure SM58 Mikrofon", SaatlikKiraUcreti = 50 },
+                new Ekipman { Id = 4, EkipmanAdi = "Yamaha Bateri Seti", SaatlikKiraUcreti = 150 }
+            };
+
+            // Kargoyu View'a fırlat
+            return View(sahteEkipmanlar);
+        }
+    }
+}
---
+++ Models/Ekipman.cs
@@ -0,0 +1,9 @@
+﻿namespace RezerveMuzikWeb.Models
+{
+    public class Ekipman
+    {
+        public int Id { get; set; }
+        public string EkipmanAdi { get; set; }
+        public decimal SaatlikKiraUcreti { get; set; }
+    }
+}
