8250a2afa1c6cd6a888fd2f71affffa320f5f510
hasanYigit <yigith@posta.mu.edu.tr>
2026-03-27 17:40:56 +0300

    Menü tasarımı veritabanı mimarisine göre güncellendi. 'Giriş Yap' butonu Flexbox ile sağ üst köşeye hizalandı. Odalar Controller ve Index View eklendi; Bootstrap Grid sistemi ve mock data kullanılarak stüdyo listeleme ekranı dinamik hale getirildi.

---
+++ Controllers/OdalarController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RezerveMuzikWeb.Models;
+using System.Collections.Generic;
+
+namespace RezerveMuzikWeb.Controllers
+{
+    public class OdalarController : Controller
+    {
+        // Ana menüden "Stüdyolarımız" sekmesine tıklandığında bu metot çalışır
+        public IActionResult Index()
+        {
+            // Veritabanı yerine geçici olarak 3 tane sahte stüdyo (oda) oluşturuyoruz
+            var sahteOdalar = new List<Oda>
+            {
+                new Oda { Id = 1, OdaAdi = "Akustik Stüdyo", Kapasite = 3, SaatlikUcret = 250 },
+                new Oda { Id = 2, OdaAdi = "Büyük Prova Odası", Kapasite = 8, SaatlikUcret = 400 },
+                new Oda { Id = 3, OdaAdi = "Vokal Kayıt Odası", Kapasite = 2, SaatlikUcret = 350 }
+            };
+
+            // Bu listeyi, ekranda (View) gösterilmesi için gönderiyoruz
+            return View(sahteOdalar);
+        }
+    }
+}
