fc528a3f344fd2fbcd5c2ede284dd06db308b5a0
hasanYigit <yigith@posta.mu.edu.tr>
2026-03-28 18:27:35 +0300

    UI/UX Mimarisi Tamamlandı: _Layout.cshtml içine dinamik arka plan mantığı (Odalar, Ekipmanlar ve Rezervasyon sayfaları için) eklendi. Rezervasyon formunun tasarımı, temanın karanlık (dark) premium konseptine uygun olarak yenilendi. HTML hiyerarşisindeki hatalar ve gereksiz etiketler temizlendi.

---
+++ Controllers/HesapController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RezerveMuzikWeb.Controllers
+{
+    public class HesapController : Controller
+    {
+        // Menüdeki "Giriş Yap" butonuna tıklandığında bu metot çalışır
+        public IActionResult Giris()
+        {
+            // Kullanıcıya giriş yapması için boş bir form (View) gönderir
+            return View();
+        }
+    }
+}
